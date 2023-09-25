2013df4f5b84c88cea9871ec326a7646b265ef0f
iamajirobamusty <110599927+iamajirobamusty@users.noreply.github.com>
2023-09-26 00:44:49 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,7 +29,7 @@ void bubble_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (swap == false)
 	{
 		swap = true;
 		for (i = 0; i < len - 1; i++)

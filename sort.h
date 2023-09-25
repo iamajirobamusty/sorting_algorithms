49825ddf1686f371762791f301873b93b9cd485c
iamajirobamusty <110599927+iamajirobamusty@users.noreply.github.com>
2023-09-26 00:55:24 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,11 +1,22 @@
 #ifndef SORT_H
 #define SORT_H
 
+
+#define UP 0
+#define DOWN 1
+
 typedef enum bool
 {
 	false = 0,
 	true
 } bool;
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);

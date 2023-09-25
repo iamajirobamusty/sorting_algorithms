5863c577508a66541dd152897e6313cdcbbc4d61
iamajirobamusty <110599927+iamajirobamusty@users.noreply.github.com>
2023-09-26 00:53:45 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,6 +1,12 @@
 #ifndef SORT_H
 #define SORT_H
 
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
